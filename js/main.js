fda6c47f9e5bb26071e568f7af3a6b290e1f6a77
JxDannyxJ <daniel.jadanec@gmx.de>
2016-12-13 22:05:54 +0100

    - Translated english output to german for presentation purpose, language boundles will be introduced later - documentation for the functions in the js files - css added to a new styled index.html

---
+++ js/main.js
@@ -0,0 +1,229 @@
+var startPoints = 40;
+var consumeBonus = 20;
+var n = 24; //atm must be dividable by 2 and 3
+var rounds = 10;
+
+storageCosts[typeA] = 1;
+storageCosts[typeB] = 4;
+storageCosts[typeC] = 9;
+
+propTypeAccepts['1' + typeA] = 1.0;
+propTypeAccepts['1' + typeB] = 1.0;
+propTypeAccepts['1' + typeC] = 0.0;
+
+propTypeAccepts['2' + typeA] = 1.0;
+propTypeAccepts['2' + typeB] = 1.0;
+propTypeAccepts['2' + typeC] = 0.0;
+
+propTypeAccepts['3' + typeA] = 1.0;
+propTypeAccepts['3' + typeB] = 0.0;
+propTypeAccepts['3' + typeC] = 1.0;
+
+propTypeNice[1] = 0.0;
+propTypeNice[2] = 0.0;
+propTypeNice[3] = 0.0;
+
+var verbose = true; // log each round
+
+/**
+ *  Resets the input fields to the initial state
+ */
+function resetInputFields(){
+    var e;
+    e = document.getElementById('storageAtxt');
+    e.value = 1;
+    e = document.getElementById('storageBtxt');
+    e.value = 4;
+    e = document.getElementById('storageCtxt');
+    e.value = 9;
+    e = document.getElementById('startPointsTxt');
+    e.value = 40;
+    e = document.getElementById('consumeBonusTxt');
+    e.value = 20;
+    e = document.getElementById('probType1acceptsA');
+    e.value = 1;
+    e = document.getElementById('probType1acceptsB');
+    e.value = 0;
+    e = document.getElementById('probType1acceptsC');
+    e.value = 0;
+    e = document.getElementById('probType2acceptsA');
+    e.value = 1;
+    e = document.getElementById('probType2acceptsB');
+    e.value = 1;
+    e = document.getElementById('probType2acceptsC');
+    e.value = 0;
+    e = document.getElementById('probType3acceptsA');
+    e.value = 1;
+    e = document.getElementById('probType3acceptsB');
+    e.value = 0;
+    e = document.getElementById('probType3acceptsC');
+    e.value = 1;
+    e = document.getElementById('propTyp1nice');
+    e.value = 0;
+    e = document.getElementById('propTyp2nice');
+    e.value = 0;
+    e = document.getElementById('propTyp3nice');
+    e.value = 0;
+};
+
+/**
+ *  Resets the input fields and sets the settings for the corresponding
+ *  trading strategy
+ */
+function presetsSelectorChanged(){
+    resetInputFields();
+    var e;
+    switch(document.getElementById('presetsSelector').value){
+        case 'strict':
+            break;
+        case 'stubborn':
+            e = document.getElementById('probType1acceptsB');
+            e.value = 0;
+            e = document.getElementById('probType2acceptsA');
+            e.value = 0;
+            e = document.getElementById('probType3acceptsA');
+            e.value = 0;
+            break;
+        case 'strategic':
+            e = document.getElementById('probType1acceptsC');
+            e.value = 1;
+            e = document.getElementById('probType3acceptsB');
+            e.value = 1;
+            break;
+        case 'social':
+            e = document.getElementById('propTyp1nice');
+            e.value = 0.25;
+            e = document.getElementById('propTyp2nice');
+            e.value = 0.25;
+            e = document.getElementById('propTyp3nice');
+            e.value = 0.25;
+            break;
+        case 'equilibrium':
+            e = document.getElementById('probType1acceptsB');
+            e.value = 1;
+            e = document.getElementById('probType1acceptsC');
+            e.value = 1;
+            e = document.getElementById('probType2acceptsC');
+            e.value = 1;
+            e = document.getElementById('probType3acceptsB');
+            e.value = 1;
+            e = document.getElementById('storageAtxt');
+            e.value = 5;
+            e = document.getElementById('storageBtxt');
+            e.value = 5;
+            e = document.getElementById('storageCtxt');
+            e.value = 5;
+            break;
+        case 'random':
+            e = document.getElementById('storageAtxt');
+            e.value = Math.round(Math.random() * 10);
+            e = document.getElementById('storageBtxt');
+            e.value = Math.round(Math.random() * 10);
+            e = document.getElementById('storageCtxt');
+            e.value = Math.round(Math.random() * 10);
+            e = document.getElementById('startPointsTxt');
+            e.value = Math.round(Math.random() * 50);
+            e = document.getElementById('consumeBonusTxt');
+            e.value = Math.round(Math.random() * 50);
+            e = document.getElementById('probType1acceptsA');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('probType1acceptsB');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('probType1acceptsC');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('probType2acceptsA');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('probType2acceptsB');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('probType2acceptsC');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('probType3acceptsA');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('probType3acceptsB');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('probType3acceptsC');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('propTyp1nice');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('propTyp2nice');
+            e.value = roundDec(Math.random(), 2);
+            e = document.getElementById('propTyp3nice');
+            e.value = roundDec(Math.random(), 2);
+            break;
+    }
+};
+
+/**
+ *  Onclick listener which initializes the variables with the values from the input fields
+ *  and starts the simulation procedure
+ */
+document.getElementById('runSimBtn').onclick = function(){
+    n = parseInt(document.getElementById('numbPersonsTxt').value);
+    if(n % 2 != 0 || n % 3 != 0)
+        notie.alert(3, 'Sorry, die Anzahl an Personen muss durch  <b>2</b> <i>and</i> <b>3</b> <small> teilbar sein </small>', 5);
+    else {
+        rounds = parseInt(document.getElementById('numbRoundsTxt').value);
+        storageCosts[typeA] = parseInt(document.getElementById('storageAtxt').value);
+        storageCosts[typeB] = parseInt(document.getElementById('storageBtxt').value);
+        storageCosts[typeC] = parseInt(document.getElementById('storageCtxt').value);
+        startPoints = parseInt(document.getElementById('startPointsTxt').value);
+        consumeBonus = parseInt(document.getElementById('consumeBonusTxt').value);
+
+        storageCosts[typeA] = parseInt(document.getElementById('storageAtxt').value);
+
+        propTypeAccepts['1' + typeA] = parseFloat(document.getElementById('probType1acceptsA').value);
+        propTypeAccepts['1' + typeB] = parseFloat(document.getElementById('probType1acceptsB').value);
+        propTypeAccepts['1' + typeC] = parseFloat(document.getElementById('probType1acceptsC').value);
+
+        propTypeAccepts['2' + typeA] = parseFloat(document.getElementById('probType2acceptsA').value);
+        propTypeAccepts['2' + typeB] = parseFloat(document.getElementById('probType2acceptsB').value);
+        propTypeAccepts['2' + typeC] = parseFloat(document.getElementById('probType2acceptsC').value);
+
+        propTypeAccepts['3' + typeA] = parseFloat(document.getElementById('probType3acceptsA').value);
+        propTypeAccepts['3' + typeB] = parseFloat(document.getElementById('probType3acceptsB').value);
+        propTypeAccepts['3' + typeC] = parseFloat(document.getElementById('probType3acceptsC').value);
+
+        propTypeNice[1] = parseFloat(document.getElementById('propTyp1nice').value);
+        propTypeNice[2] = parseFloat(document.getElementById('propTyp2nice').value);
+        propTypeNice[3] = parseFloat(document.getElementById('propTyp3nice').value);
+
+        verbose = document.getElementById('loggingCheckbox').checked;
+
+        runSim();
+    }
+};
+
+/**
+ * Initializes the experiment with the given settings and runs the simulation and analysis
+ */
+function runSim(){
+    print('<b><font size="5" color="green">Initialisiere...</font></b>');
+    print();
+    print('Gesamtanzahl der Personen = ' + n);
+    print('Runden = ' + rounds);
+    print('Lagerkosten = ' + typeA + ': ' + storageCosts[typeA] + ', ' + typeC + ': ' + storageCosts[typeB] + ', ' + typeC + ': ' + storageCosts[typeC]);
+    print('Anfangspunktzahl = ' + startPoints)
+    print('Konsumbonus = ' + consumeBonus + '</font>');
+    print('Gesamtzahl an Gütern = ' + n);
+    print('Wahrscheinlichkeit von Bauer Gut zu akzeptieren ' + typeA + ': ' + propTypeAccepts['1' + typeA] + ', ' + typeB +':' + propTypeAccepts['1' + typeB] + ', ' + typeC + ': ' + propTypeAccepts['1' + typeC]);
+    print('Wahrscheinlichkeit von Müller Gut zu akzeptieren ' + typeA + ': ' + propTypeAccepts['2' + typeA] + ', ' + typeB +':' + propTypeAccepts['2' + typeB] + ', ' + typeC +':' + propTypeAccepts['2' + typeC]);
+    print('Wahrscheinlichkeit von Bäcker Gut zu akzeptieren ' + typeA + ': ' + propTypeAccepts['3' + typeA] + ' ' + typeB +':' + propTypeAccepts['3' + typeB] + ', ' + typeC +':' + propTypeAccepts['3' + typeC]);
+    print('Wahrscheinlichkeit, dass Bauer entgegenkommend ist: ' + propTypeNice[1]);
+    print('Wahrscheinlichkeit, dass Müller entgegenkommend ist: : ' + propTypeNice[2]);
+    print('Wahrscheinlichkeit, dass Bäcker entgegenkommend ist: ' + propTypeNice[3]);
+    createPersons();
+    print('Anzahl an Personen = Bauer: ' + personTypeAmounts[1] + ', Müller: ' + personTypeAmounts[2] + ', Bäcker: ' + personTypeAmounts[3]);
+    print('Anzahl an Gütern = ' + typeA +':' + goodsStartAmounts[typeA] + ', ' + typeB +':' + goodsStartAmounts[typeB] + ', ' + typeC +':' + goodsStartAmounts[typeC] );
+    print();
+    print('<b><font size="5" color="blue">Starte Simulation</font></b>');
+
+    runRounds(rounds);
+    if(!verbose){
+        print();
+        print('<i><font color="gray"><small>[output not shown]</small></font></i>');
+    }
+    print('<br><b><font size="5" color="blue">Simulation beendet</font></b>');
+    print('<br><b><font size="5" color="red">Markanalyse</font></b><br>');
+    runAnalysis();
+    print();
+};
--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ var storageCosts = {};
 var persons = [];
 var propTypeAccepts = {};
 var propTypeNice = {};
+var showSingleSteps = true;
+var buttonPressed = false;
+var typeA = 'Brot';
+var typeB = 'Getreide';
+var typeC = 'Mehl';
 
 var personTypeAmounts = {};
 personTypeAmounts[1] = 0;
@@ -10,21 +15,21 @@ personTypeAmounts[2] = 0;
 personTypeAmounts[3] = 0;
 
 var goodsStartAmounts = {};
-goodsStartAmounts['A'] = 0;
-goodsStartAmounts['B'] = 0;
-goodsStartAmounts['C'] = 0;
+goodsStartAmounts[typeA] = 0;
+goodsStartAmounts[typeB] = 0;
+goodsStartAmounts[typeC] = 0;
 
 var goodsEndAmounts = {};
-goodsEndAmounts['A'] = 0;
-goodsEndAmounts['B'] = 0;
-goodsEndAmounts['C'] = 0;
+goodsEndAmounts[typeA] = 0;
+goodsEndAmounts[typeB] = 0;
+goodsEndAmounts[typeC] = 0;
 
 
 function getDecision(yesThreshold){ // true is yes, false is no
     if(yesThreshold == 0)
         return false;
-    if(yesThreshold == 1)    
-        return true;   
+    if(yesThreshold == 1)
+        return true;
     return Math.random() <= yesThreshold;
 };
 
@@ -41,23 +46,23 @@ var Person = function(id, type, consume, produce){
 };
 
 Person.prototype = {
-    
+
     roundStart: function(){
-        this.pointsBeforeRound = this.points;    
+        this.pointsBeforeRound = this.points;
     },
-    
+
     resetPointsDelta: function(){
         this.pointsDelta = 0;
     },
-    
+
     wantTrade: function(offeredGood){
         return getDecision(propTypeAccepts[this.type + offeredGood]);
     },
-    
+
     makeConcession: function(){
         return getDecision(propTypeNice[this.type]);
     },
-    
+
     doTrade: function(incomingGood){
         if(incomingGood == this.consume){
             this.good = this.produce; //=incomingGood
@@ -66,30 +71,30 @@ Person.prototype = {
         else
             this.good = incomingGood;
     },
-    
+
     payStorage: function(){
         this.points -= storageCosts[this.good];
         this.pointsDelta = this.points - this.pointsBeforeRound;
     },
-    
+
     getIdStr: function(){
         return this.id < 10 ? '0' + this.id : '' + this.id;
     },
-    
+
     getDetails: function(){
         return '<i>[Person' + this.getIdStr() + ', type: <b>' + this.type + '</b>, good: <b>' + this.good + '</b>]</i>';
     },
-    
+
     toString: function(){ //short toString
         return '<i>Person' + this.getIdStr() + '</i>';
     },
-    
+
     getDeltaPointsStr: function(){
         if(this.pointsDelta == 0)
             return '';
         return '<small>(' + (this.pointsDelta < 0 ? '' + this.pointsDelta : '+' + this.pointsDelta) + ')</small>';
     },
-    
+
     getStatus: function(){
         return '<i>Person' + this.getIdStr() + ', type: <b>' + this.type + '</b>, good: <b>' + this.good + '</b>, points: <b>' + this.points + '</b>' + this.getDeltaPointsStr() + '</i>';
     }
@@ -101,73 +106,76 @@ function createPersonTypeGroup(numb, personType, consumeGood, produceGood){
         var p = new Person(persons.length + 1, personType, consumeGood, produceGood);
         persons.push(p);
         personTypeAmounts[personType] += 1;
-        print('created ' + p.getDetails(), true);
+        print(p.getDetails() + ' erstellt!', true);
     };
 };
 
+/**
+ *  Create persons of each type equally distributed
+ */
 function createPersons(){
     print('<small>', true);
     var amountOfEachType = n / 3;
-    createPersonTypeGroup(amountOfEachType, 1, 'A', 'B');
-    createPersonTypeGroup(amountOfEachType, 2, 'B', 'C');
-    createPersonTypeGroup(amountOfEachType, 3, 'C', 'A');
+    createPersonTypeGroup(amountOfEachType, 1, typeA, typeB);
+    createPersonTypeGroup(amountOfEachType, 2, typeB, typeC);
+    createPersonTypeGroup(amountOfEachType, 3, typeC, typeA);
     print('</small>', true);
 };
 
 
 function noTrade(){
-    return '<font color="red"><b>no</b></font>, ';
+    return '<font color="red"><b>Nein</b></font>, ';
 };
 
 function yesTrade(){
-    return '<font color="green"><b>yes</b></font>, ';
+    return '<font color="green"><b>Ja</b></font>, ';
 };
 
 
 function encounter(p1, p2){
-    var log = 'encounter of ' + p1.getDetails() + ' and ' + p2.getDetails() + '&nbsp;&nbsp;&nbsp;<font size="2" color="gray">>></font>&nbsp;&nbsp;';
+    var log = p1.getDetails() + ' und ' + p2.getDetails() + ' handeln' + '&nbsp;&nbsp;&nbsp;<font size="2" color="gray">>></font>&nbsp;&nbsp;';
     if(p1.good == p2.good)
-        log += noTrade() + "both have the same good to offer";
+        log += noTrade() + "beide bieten das gleiche Konsumgut an";
     else {
         var p1wantsTrade = p1.wantTrade(p2.good);
         var p2wantsTrade = p2.wantTrade(p1.good);
-        
+
         var doTrade = p1wantsTrade && p2wantsTrade;
         if(doTrade)
-            log += yesTrade() + "trade takes place because both want to";
-        
+            log += yesTrade() + "der Handel wird von beiden Seiten akzeptiert";
+
         if(!p1wantsTrade && !p2wantsTrade)
-            log += noTrade() + "both don't want to";
-            
+            log += noTrade() + "der Handel wird von beiden Seiten abgelehnt";
+
         if(p1wantsTrade && !p2wantsTrade){
             if(p2.makeConcession()){
-                log += yesTrade() + p1 + " wants to and " + p2 + " didn't want originally but is willing to make the trade anyway";
+                log += yesTrade() + p1 + " möchte handeln, während " + p2 + " nicht handeln wollte, aber " + p1 + " entgegenkommt!";
                 doTrade = true;
             }
             else
-                log += noTrade() + p1 + " wants to but " + p2 + " doesn't";
+                log += noTrade() + p1 + " möchte handeln, aber " + p2 + " möchte es nicht!";
         }
-        
+
         if(!p1wantsTrade && p2wantsTrade){
             if(p1.makeConcession()){
-                log += yesTrade() + p2 + " wants to and " + p1 + " didn't want originally but is willing to make the trade anyway";
+                log += yesTrade() + p2 + " möchte handeln, während " + p1 + " nicht handeln wollte, aber " + p2 + " entgegenkommt!";
                 doTrade = true;
             }
             else
-                log += noTrade() + p2 + " wants to but " + p1 + " doesn't";
+                log += noTrade() + p2 + " möchte handeln, aber " + p1 + " möchte es nicht!";
         }
-        
+
         if(doTrade){
             var p1_good = p1.good;
             p1.doTrade(p2.good);
-            p2.doTrade(p1_good);    
+            p2.doTrade(p1_good);
         }
     }
-    print(log, true);     
+    print(log, true);
 };
 
 
-function showStatuses(analysisMode){    
+function showStatuses(analysisMode){
     for(var i in persons){
         var lineNumb = parseInt(i) < 9 ? '&nbsp;&nbsp;' + (parseInt(i) + 1) : '' + (parseInt(i) + 1);
         print((analysisMode ? '<font size="2" color="gray">' + lineNumb + ':</font>&nbsp;&nbsp;' : '') + persons[i].getStatus(), analysisMode ? undefined : true);
@@ -175,7 +183,12 @@ function showStatuses(analysisMode){
     };
 };
 
-
+/**
+ * Runs a trading round for the persons and calculates
+ * the storage costs and the total amount of points for
+ * each person, which will be printed in the status part
+ * @param roundNumb amount of rounds to run
+ */
 function oneRound(roundNumb){
     for(var i in persons)
         persons[i].roundStart();
@@ -183,56 +196,81 @@ function oneRound(roundNumb){
     print('<b><font size="4" color="green">round ' + roundNumb + '</font></b>', true);
     var indize = [];
     for(var i = 0; i < n; i ++)
-        indize.push(i);    
+        indize.push(i);
     for(var i = 0; i < n / 2; i ++){
-        var randIndex = Math.abs(Math.round(Math.random() * indize.length - 0.5));    
+        var randIndex = Math.abs(Math.round(Math.random() * indize.length - 0.5));
         var p1 = persons[indize.splice(randIndex, 1)];
-        randIndex = Math.abs(Math.round(Math.random() * indize.length - 0.5));    
+        randIndex = Math.abs(Math.round(Math.random() * indize.length - 0.5));
         var p2 = persons[indize.splice(randIndex, 1)];
         encounter(p1, p2);
     };
     for(var i in persons)
         persons[i].payStorage();
-    print('<font color="gray">status at end of round ' + roundNumb + ':</font>', true);
+    print('<font color="gray">Status am ende der Runde ' + roundNumb + ':</font>', true);
     showStatuses(false);
 };
 
 function runRounds(rounds){
-    for(var i = 0; i < rounds; i ++)
+    // if (showSingleSteps) {
+    //     registerHandler();
+    // }
+    for(var i = 0; i < rounds; i ++) {
         oneRound(i + 1);
+        // if (showSingleSteps) {
+        //     waitForIt();
+        // }
+    }
 };
 
+function keydownHandler(e) {
+    if (e.keyCode == 13) {  // 13 is the enter key
+        buttonPressed = true;
+    }
+};
 
+function registerHandler() {
+    // register your handler method for the keydown event
+    if (document.addEventListener) {
+        document.addEventListener('keydown', keydownHandler, false);
+    }
+    else if (document.attachEvent) {
+        document.attachEvent('onkeydown', keydownHandler);
+    }
+}
+
+/**
+ *
+ */
 function runAnalysis(){
-    print('<font color="gray">final status, <b>sorted</b>:</font>');
+    print('<font color="gray">Finaler Status, <b>geordnet</b>:</font>');
     persons.sort(function sortFunc(p1, p2){return p2.points - p1.points;});
-    showStatuses(true); 
-    
+    showStatuses(true);
+
     for(var i in persons)
         goodsEndAmounts[persons[i].good] += 1;
-    
-    var goodsPercentages = {};
-    goodsPercentages['A_start'] = roundDec((goodsStartAmounts['A'] / n) * 100, 1) + '%';
-    goodsPercentages['B_start'] = roundDec((goodsStartAmounts['B'] / n) * 100, 1) + '%';
-    goodsPercentages['C_start'] = roundDec((goodsStartAmounts['C'] / n) * 100, 1) + '%';
-    goodsPercentages['A_end'] = roundDec((goodsEndAmounts['A'] / n) * 100, 1) + '%';
-    goodsPercentages['B_end'] = roundDec((goodsEndAmounts['B'] / n) * 100, 1) + '%';
-    goodsPercentages['C_end'] = roundDec((goodsEndAmounts['C'] / n) * 100, 1) + '%';
-    goodsPercentages['A_diff'] = roundDec((goodsEndAmounts['A'] / goodsStartAmounts['A']) * 100, 1) + '%';
-    goodsPercentages['B_diff'] = roundDec((goodsEndAmounts['B'] / goodsStartAmounts['B']) * 100, 1) + '%';
-    goodsPercentages['C_diff'] = roundDec((goodsEndAmounts['C'] / goodsStartAmounts['C']) * 100, 1) + '%';
 
-    printGoodPerformance(goodsPercentages, 'A');
-    printGoodPerformance(goodsPercentages, 'B');
-    printGoodPerformance(goodsPercentages, 'C');
+    var goodsPercentages = {};
+    goodsPercentages[typeA + '_start'] = roundDec((goodsStartAmounts[typeA] / n) * 100, 1) + '%';
+    goodsPercentages[typeB + '_start'] = roundDec((goodsStartAmounts[typeB] / n) * 100, 1) + '%';
+    goodsPercentages[typeC + '_start'] = roundDec((goodsStartAmounts[typeC] / n) * 100, 1) + '%';
+    goodsPercentages[typeA + '_end'] = roundDec((goodsEndAmounts[typeA] / n) * 100, 1) + '%';
+    goodsPercentages[typeB + '_end'] = roundDec((goodsEndAmounts[typeB] / n) * 100, 1) + '%';
+    goodsPercentages[typeC + '_end'] = roundDec((goodsEndAmounts[typeC] / n) * 100, 1) + '%';
+    goodsPercentages[typeA + '_diff'] = roundDec((goodsEndAmounts[typeA] / goodsStartAmounts[typeA]) * 100, 1) + '%';
+    goodsPercentages[typeB + '_diff'] = roundDec((goodsEndAmounts[typeB] / goodsStartAmounts[typeB]) * 100, 1) + '%';
+    goodsPercentages[typeC + '_diff'] = roundDec((goodsEndAmounts[typeC] / goodsStartAmounts[typeC]) * 100, 1) + '%';
+
+    printGoodPerformance(goodsPercentages, typeA);
+    printGoodPerformance(goodsPercentages, typeB);
+    printGoodPerformance(goodsPercentages, typeC);
 };
 
 
 function printGoodPerformance(goodsPercentages, good){
     print();
-    print('<font color="green">performance of <b>good ' + good + '</b> on the market:</font>');
+    print('<font color="green">Marktanteil von <b>' + good + '</b>:</font>');
     if(goodsStartAmounts[good] == goodsEndAmounts[good])
-        print('stayed at <b>' + goodsStartAmounts[good] + '</b> <small>(' + goodsPercentages[good + '_start'] + ' of all goods)</small>');
+        print('Blieb bei <b>' + goodsStartAmounts[good] + '</b> <small>(' + goodsPercentages[good + '_start'] + ' von allen Gütern)</small>');
     else
-        print('went from <b>' + goodsStartAmounts[good] + '</b> <small>(' + goodsPercentages[good + '_start'] + ' of all goods)</small> to <b>' + goodsEndAmounts[good] + '</b> <small>(' + goodsPercentages[good + '_end'] + " of all goods)</small>, that's <b>" + goodsPercentages[good + '_diff'] + '</b> of the initial ' + goodsStartAmounts['A']);
+        print('Veränderte sich von <b>' + goodsStartAmounts[good] + '</b> <small>(' + goodsPercentages[good + '_start'] + ' von allen Gütern)</small> auf <b>' + goodsEndAmounts[good] + '</b> <small>(' + goodsPercentages[good + '_end'] + " von allen Gütern)</small>, das ist <b>" + goodsPercentages[good + '_diff'] + '</b> von der anfänglichen ' + goodsStartAmounts[good]);
 };
